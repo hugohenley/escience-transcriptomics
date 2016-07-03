b9eb45f054f8ec46938b7654c149d7a7c188fab5
Hugo Henley <hugohenley@id.uff.br>
2016-07-02 23:18:03 -0300

    Apontando script direto para a pasta que contem o featureCounts

--- feature_counts.py
+++ feature_counts.py
@@ -1,5 +1,5 @@
 import glob, os
-command = "featureCounts -T 4 -O -R --primary -F GTF -p -a Drosophila_melanogaster.BDGP5.70.gtf -o teste.table"
+command = "/home/hugo/storage/TrabalhoFinal_eScience_shell/subread-1.5.0-p3-Linux-x86_64/bin/featureCounts -T 4 -O -R --primary -F GTF -p -a Drosophila_melanogaster.BDGP5.70.gtf -o teste.table"
 for file in glob.glob("*.sort.bam"):
   command += ' ' + file
 os.system(command)
